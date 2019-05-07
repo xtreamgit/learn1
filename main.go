05174382eca105dd509cc2887c6a696b33342f57
Hector DeJesus <hector@mergingstreams.com>
2019-05-07 12:42:19 -0700

    This is the first commit for the learn1 methods practice program. I've included the example of an embedded type.

---
+++ main.go
@@ -0,0 +1,59 @@
+package main
+
+import "fmt"
+
+// In this go program I will learn
+// composition
+// embedded types
+// inner-type promotion
+// methods
+// interfaces
+
+type robot struct {
+	firstName string
+	lastName  string
+}
+
+type fantasticRobot struct {
+	robot
+	workArea   string
+	bodyColor  string
+	walkFreely bool
+	bodyShape  string
+}
+
+// This is the speak method for the robot type
+// somerobot is the "receiver" for the method
+// Anything of type robot, will be able to use the speak() method.
+func (r robot) speak() {
+	fmt.Println("I'm an Artificial Inteligence Robot. My first name is", r.firstName, "and my last name is", r.lastName)
+}
+
+func (fr fantasticRobot) speak() {
+	fmt.Println("Hey, I'm a fantastic robot. My name is", fr.firstName)
+}
+
+func main() {
+	robot1 := robot{
+		"htrom2",
+		"weekie",
+	}
+
+	// When I want the robot to speak, I call the method "speak()" with the object of type robot.
+	// In this case the object is robot1.
+	robot1.speak()
+
+	////////////////////////////////////////////
+
+	fantasticRobot1 := fantasticRobot{
+		robot{
+			"Brim5",
+			"blinto",
+		},
+		"museum",
+		"oragepeel",
+		false,
+		"tall",
+	}
+	fantasticRobot1.speak()
+}
